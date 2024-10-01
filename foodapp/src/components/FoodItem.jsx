45ba762c2b4e545ea45c18c55d5bb3ea1013c950
Awopeju Kolawole Moses <Awopejumoses@gmail.com>
2024-10-01 08:22:54 +0100

    Updated styling

--- foodapp/src/components/FoodItem.jsx
+++ foodapp/src/components/FoodItem.jsx
@@ -1,7 +1,9 @@
+import styles from './fooditem.module.css';
+
 export default function FoodItem({ food }) {
     return(
-        <div>
-            <img src={food.strMealThumb} />
+        <div className={styles.itemBox} >
+            <img className={styles.imgBox} src={food.strMealThumb} />
             <h3>{food.strMeal}</h3>
             <button>View recipe</button>
         </div>
